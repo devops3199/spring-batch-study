90854e5d479649f0ff92d02908ca9e8446bd495c
Ray Jeong <devops3199@gmail.com>
2024-10-01 15:56:46 +0900

    [chore] setup

---
+++ src/test/java/dev/ray/adbatch/AdBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ray.adbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
